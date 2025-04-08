308a868ce656c0169a52cec188d33fbe3fdb20ee
anna-viktoria23 <anna.v.carvalho@aluno.senai.br>
2025-04-08 08:45:22 -0300

    criar o arquivo
    criei o arquivo e coloquei o codigo

---
+++ projeto_matematica/lição_matematica.py
@@ -0,0 +1,22 @@
+print("Olá usuário!eu irei lhe dar o resultado de uma opreção do seu desejo")
+n1= int(input("Digite o primeiro valor: "))
+
+op= input("""Coloque a opereção desejada podendo ser: '+' para soma, '/' para divisão
+'*' para multiplicação e '-' para subtração: """)
+
+def opereções (op):
+    global n1, n2
+    if op == "+":
+        
+        print ("O resultado da sua soma é de: ", soma)
+    elif op == "-":
+        subtração= n1 - n2
+        print ("O resultado da sua subtração é de: ", subtração)
+   
+        multiplicação= n1 * n2
+        print ("O resultadp da multiplicação é de: ", multiplicação)
+    elif op == "/":
+       
+       
+
+opereções (op)
