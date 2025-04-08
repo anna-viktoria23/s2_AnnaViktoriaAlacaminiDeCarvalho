822fcb709e3c73f2886e3f7ddcd48bc17d1662de
anna-viktoria23 <anna.v.carvalho@aluno.senai.br>
2025-04-08 08:53:18 -0300

    fazer o codigo
    colocar e revisar o codigo

--- projeto_matematica/lição_matematica.py
+++ projeto_matematica/lição_matematica.py
@@ -1,22 +1,22 @@
-print("Olá usuário!eu irei lhe dar o resultado de uma opreção do seu desejo")
+print("Olá usuário! eu irei lhe dar o resultado de uma opreção do seu desejo")
 n1= int(input("Digite o primeiro valor: "))
-
+n2= int(input("Digite o segundo valor: "))
 op= input("""Coloque a opereção desejada podendo ser: '+' para soma, '/' para divisão
 '*' para multiplicação e '-' para subtração: """)
 
 def opereções (op):
     global n1, n2
     if op == "+":
-        
+        soma= n1+n2
         print ("O resultado da sua soma é de: ", soma)
     elif op == "-":
         subtração= n1 - n2
         print ("O resultado da sua subtração é de: ", subtração)
-   
+    elif op == "*":
         multiplicação= n1 * n2
         print ("O resultadp da multiplicação é de: ", multiplicação)
     elif op == "/":
-       
-       
+        divisão= n1/n2
+        print ("O resultado da divisão é de: ", divisão)
 
 opereções (op)
